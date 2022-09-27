2612e7d0bb0c3f0beeaae8e8451e8ff392fb3730
imhawool <ektmfrlehdrh@gmail.com>
2022-09-27 13:14:45 +0900

    commit

---
+++ customer-center/src/main/java/labshopcqrs/domain/DeliveryStarted.java
@@ -0,0 +1,15 @@
+package labshopcqrs.domain;
+
+import java.util.*;
+import labshopcqrs.infra.AbstractEvent;
+import lombok.Data;
+
+@Data
+public class DeliveryStarted extends AbstractEvent {
+
+    private Long id;
+    private String address;
+    private String customerId;
+    private Integer quantity;
+    private Long orderId;
+}
---
+++ order/src/main/java/labshopcqrs/external/Inventory.java
@@ -0,0 +1,13 @@
+package labshopcqrs.external;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class Inventory {
+
+    private Long id;
+    private Long stock;
+    // keep
+
+}
